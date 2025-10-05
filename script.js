27b9a0ae1bb91d7a15b6da87956628eb68b9d0b9
3elwa00 <114284893+3elwa00@users.noreply.github.com>
2025-10-05 22:04:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            // Optional: remove the class to re-animate on scroll up
+            // entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
